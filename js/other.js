f98bb539a2925535ee459dd5b8d9f0ea1aa6ed0d
Kxawzitan <outrunner77@live.co.uk>
2022-11-29 20:40:15 +0000

    Update other.js

--- js/other.js
+++ js/other.js
@@ -43,16 +43,22 @@ const POPUP_GROUPS = {
     help: {
         html: `
         <h1>Mass</h1><br>
-        g (gram): 1 g<br>
-        kg (kilogram): 1,000 g<br>
-        tonne (tonne): 1,000 kg = 1,000,000 g<br>
-        MME (mass of Mount Everest): 1.619e14 tonne = 1.619e20 g<br>
-        M⊕ (mass of Earth): 36,886,967 MME = 5.972e27 g<br>
-        M☉ (mass of Sun): 333,054 M⊕ = 1.989e33 g<br>
-        MMWG (mass of Milky Way Galaxy): 1.5e12 M☉ = 2.9835e45 g<br>
-        uni (mass of Universe): 50,276,520,864 MMWG = 1.5e56 g<br>
-        mlt (mass of Multiverse): 1e1e9 uni (logarithmic)<br>
-        mgv (mass of Megaverse): 1e15 mlt<br>
+        g (gram):                                     1 g<br>
+        kg (kilogram):                                1,000 g<br>
+        tonne (tonne):                                1,000 kg = 1,000,000 g<br>
+        MME (mass of Mount Everest):                  1.619e14 tonne = 1.619e20 g<br>
+        M⊕ (mass of Earth):                           36,886,967 MME = 5.972e27 g<br>
+        M☉ (mass of Sun):                             333,054 M⊕ = 1.989e33 g<br>
+        MMWG (mass of Milky Way Galaxy):              1.5e12 M☉ = 2.9835e45 g<br>
+        uni (mass of Universe):                       50,276,520,864 MMWG = 1.5e56 g<br>
+        mlt (mass of Multiverse):                     1e1e9 uni (logarithmic)<br>
+        mgv (mass of Megaverse):                      1e15 mlt<br>
+        giv (mass of Gigaverse):                      1e15 mgv<br>
+        tev (mass of Teraverse):                      1e15 giv<br>
+        pev (mass of Petaverse):                      1e15 tev<br>
+        exv (mass of Exaverse):                       1e15 pev<br>
+        zev (mass of Zettaverse):                     1e15 exv<br>
+        yov (mass of Yottaverse):                     1e15 zev<br>
         `,
     },
     fonts: {
